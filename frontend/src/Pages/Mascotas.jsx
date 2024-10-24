9163364f41b19bc226e426e03b5b9fa80631251e
marcelYepes <marcelyepesqf@gmail.com>
2024-10-24 10:30:36 -0500

    adecuación de estilos del footer

--- frontend/src/Pages/Mascotas.jsx
+++ frontend/src/Pages/Mascotas.jsx
@@ -4,7 +4,9 @@ import Header from "../components/Header";
 const Mascotas = () => {
   return (
     <>
-      <Header />
+      <div className="container">
+        <Header />
+      </div>
     </>
   );
 };
