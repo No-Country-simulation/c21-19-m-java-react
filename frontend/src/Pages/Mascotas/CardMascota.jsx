997b3513f67f52853ba9fab4a6f4df080be29bbb
marcelYepes <marcelyepesqf@gmail.com>
2024-10-25 17:19:08 -0500

    solucionando vista grid en mascotas

--- frontend/src/Pages/Mascotas/CardMascota.jsx
+++ frontend/src/Pages/Mascotas/CardMascota.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { urlMascota } from "../../utils/urls";
 import { getDatos } from "../../utils/apiHandler";
-import InfoMascota from "./InfoMascota";
 
 const CardMascota = () => {
   const [mascotas, setMascotas] = useState([]);
@@ -19,7 +18,32 @@ const CardMascota = () => {
   }, []);
 
   return (
-    <div>
+    <div className="container">
+      <div className="row align-items-lg-center">
+        {mascotas.map((mascota, index) => (
+          <div key={index} className="col-12 col-sm-6 col-lg-3 d-flex">
+            <article className="card-mascota mx-auto mb-3 d-flex flex-column">
+              <header className="mascota-header">
+                <img
+                  className="mascota-img"
+                  src="/perro-1.jpg"
+                  alt="foto de animal"
+                />
+              </header>
+              <div className="mascota-body">
+                <h3 className="mascota-title">{mascota.nombre}</h3>
+                <h5 className="mascota-raza">
+                  <span>Raza: </span>
+                  {mascota.raza}
+                </h5>
+              </div>
+            </article>
+          </div>
+        ))}
+      </div>
+    </div>
+
+    /*     <div>
       {mascotas.map((mascota, index) => {
         return (
           <InfoMascota
@@ -29,7 +53,7 @@ const CardMascota = () => {
           />
         );
       })}
-    </div>
+    </div> */
   );
 };
 
--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,7 +1,6 @@
 export const urlAdmin =
   "https://ct0dbl9r.use2.devtunnels.ms:8080/colitasFelices/admin";
-export const urlMascota =
-  "https://ct0dbl9r.use2.devtunnels.ms:8080/colitasFelices/mascotas";
+export const urlMascota = "http://localhost:8080/colitasFelices/mascotas";
 export const urlUsuario =
   "https://ct0dbl9r.use2.devtunnels.ms:8080/colitasFelices/usuarios";
 export const urlRegistro =
