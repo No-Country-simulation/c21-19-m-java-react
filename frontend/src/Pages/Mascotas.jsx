2c8eb5045efe2efd0c4e823e1c32245d6d3e3290
Daniel Alejandro Romero <alejandro40000@hotmail.com>
2024-10-25 09:52:45 -0500

    Se agregan datos de mascotas consumiendo la api

--- frontend/src/Pages/Mascotas.jsx
+++ frontend/src/Pages/Mascotas.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
+import { getDatos } from "../utils/apiHandler";
+import { urlMascota } from "../utils/urls";
 
 const Mascotas = () => {
+
+  const [mascotas, setMascotas] = useState([]);
+
+  useEffect(() => {
+    const obtenerDatos = async() => {
+      try {        
+        const datos = await getDatos(urlMascota);
+        console.log(datos)
+        setMascotas(datos);
+      } catch (error) {
+        console.error("Error al obtener los datos:", error)
+      }
+    }
+    obtenerDatos();
+  },[]);
+
   return (
     <>
       <Header />
+      {
+        mascotas.map((mascota, index) => (
+          <div key={index}>
+            Imagen:{mascota.imagen}
+            Nombre:{mascota.nombre} 
+            <div> Categoria
+              Raza:{mascota.raza} 
+              Especie:{mascota.especie} 
+            </div>
+            Descripcion:{mascota.descripcion}
+            Medida: {mascota.medida}
+          </div>
+        ))
+      }
     </>
   );
 };
